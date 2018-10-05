7dcb420c526360fa6166d308375bee9ea704ae3d
Kvinto1986 <kvinto1986@gmail.com>
2018-10-05 10:15:44 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 module.exports = function longestConsecutiveLength(array) {
-    var result=0;
-        var best=1;
-        var min=0;
-        array.sort(function(a, b) {
-            return a - b;
-        })
+    let result = 0;
+    let best = 1;
+    let min = 0;
+    array.sort = (a, b) =>
+        a - b;
 
-            for(var j=0;j<array.length;j++){
-                min=array[j]
+    for (let i = 0; i < array.length; i++) {
+        min = array[i]
 
 
-                if(array.length<1){
-                    break
-                }
-                if(array.length==1){
-                    result=1
-                    break
-                }
-                if (array[j+1]==min+1) {
-                    best++
-
-                }
-
-                else if(array[j+1]>min+1){
-                    best=1;
-
-                }
-                if (best>result){
-                    result=best
-                }
-
-            }
-            return result
+        if (array.length < 1) {
+            break
         }
+        if (array.length === 1) {
+            result = 1
+            break
+        }
+        if (array[i + 1] === min + 1) {
+            best++
+
+        }
+
+        else if (array[i + 1] > min + 1) {
+            best = 1;
+
+        }
+        if (best > result) {
+            result = best
+        }
+
+    }
+    return result
+}
 
 
 
